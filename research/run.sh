32c3d2e2df2e2371f5ce79ed439535c395d8e64f
johniao <johniao@163.com>
2019-07-01 17:09:52 +0800

    1

--- research/run.sh
+++ research/run.sh
@@ -13,7 +13,7 @@ output_dir=/output  # 训练目录
 dataset_dir=/data/lg50854656/product # 数据集目录，这里是写死的，记得修改
 
 model_dir=$output_dir
-checkpoint_dir=$train_dir
+train_dir=$output_dir/train
 NUM_TRAIN_STEPS = 200000
 eval_dir=$output_dir/eval
 
@@ -41,7 +41,7 @@ do
 done
 
 # 导出模型
-python ./object_detection/export_inference_graph.py --input_type image_tensor --pipeline_config_path $pipeline_config_path --trained_checkpoint_prefix $train_dir/model.ckpt-$current  --output_directory $output_dir/exported_graphs
+#python ./object_detection/export_inference_graph.py --input_type image_tensor --pipeline_config_path $pipeline_config_path --trained_checkpoint_prefix $train_dir/model.ckpt-$current  --output_directory $output_dir/exported_graphs
 
 # 在test.jpg上验证导出的模型
 # python ./inference.py --output_dir=$output_dir --dataset_dir=$dataset_dir
